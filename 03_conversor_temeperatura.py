d637b5ad71c2308821960a855ee2c5d85d6afdbf
neobraingit <neobrain@protonmail.com>
2024-06-16 18:10:57 +0200

    03_conversor_temeperaturas

---
+++ 03_conversor_temeperatura.py
@@ -0,0 +1,11 @@
+
+'''
+Conversor de temeperatura:
+Escribe un programa que convierta Celsius a Fahrenheit. 
+'''
+
+celsius = float(input('Introduce los grados Celsius que deseas convertir: '))
+
+fahrenheit = (celsius * 9**5) + 32
+
+print (f'Los grados Celsius convertidos son: {fahrenheit} fahrenheit')
