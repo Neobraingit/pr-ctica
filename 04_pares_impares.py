58120bd7af5262f043ad264758f9cda8b88e97e1
neobraingit <neobrain@protonmail.com>
2024-06-16 18:15:48 +0200

    04_par_impar

---
+++ 04_pares_impares.py
@@ -0,0 +1,13 @@
+
+'''
+Comprobador de Pares e impares:
+Escribe un programa que compruebe si un número dado es par o impar
+'''
+
+def comprobador(numero):
+    if numero % 2 == 0:
+        print ('El número es par.')
+    else:
+        print ('El número es impar.')
+        
+comprobador(3)
