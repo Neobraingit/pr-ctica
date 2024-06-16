4f022d6ea2a7c8c6e8ad54aa32cd7d71d76837dd
neobraingit <neobrain@protonmail.com>
2024-06-16 20:39:56 +0200

    08_tasa_interes

---
+++ 08_calculadora_interes_simple.py
@@ -0,0 +1,14 @@
+
+'''
+Calculadora de interés simple:
+Calcula el interés sdimple para una cantidad, una tasa de interés y un número de años dados.
+'''
+
+cantidad = float(input('Introduce una cantidad: '))
+tasa_interes = float(input('Introduce la tasa de interés: '))
+numero_anos = int(input('Introduce el número de años: '))
+
+interes = (cantidad * tasa_interes * numero_anos)
+print (interes)
+
+
