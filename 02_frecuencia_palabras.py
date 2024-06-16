66c9f655b811316c72a6d68d4510a4862fe0e772
neobraingit <neobrain@protonmail.com>
2024-06-16 18:06:10 +0200

    02_frecuencia_palabras

--- 02_frecuencia_palabras.py
+++ 02_frecuencia_palabras.py
@@ -3,14 +3,27 @@
 Escribe un programa que tome una cadena de texto como entrada y cuente la frecuencia de cada palabra en la cadena. 
 '''
 
+def contador():
+    cadena = input('Introduce la cadena de texto: ')
+    palabras = cadena.lower().split()
+    contador_palabras = {}
+    
+    for palabra in palabras:
+        if palabra in contador_palabras:
+            contador_palabras[palabra] += 1
+        else:
+            contador_palabras[palabra] = 1
+    print (f'Frecuencia de palabras: ', contador_palabras)
 
-cadena = input('Introduce la cadena: ')
-palabras = cadena.lower().split()
-contador_palabras = {}
-
-for palabra in palabras:
-    if palabra in contador_palabras:
-        contador_palabras[palabra] += 1
+    
+    
+    
+    
+while True:
+    print ('1) Contar frecuencia de palabras')
+    opcion  = input('Introduce tu opción: ')
+    if opcion == '1':
+        contador()
     else:
-        contador_palabras[palabra] = 1
-print ('Frecuencia de palabras: ', contador_palabras)
+        print ('Introduce una opción válida')
+    
