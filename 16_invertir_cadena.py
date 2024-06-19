9dd22712c0b0aa954579107cf468b972483b62f7
neobraingit <neobrain@protonmail.com>
2024-06-19 20:20:28 +0200

    16_invertir_cadena

---
+++ 16_invertir_cadena.py
@@ -0,0 +1,9 @@
+
+'''
+Invertir una cadena de texto:
+Escribe un programa que invierta una cadena de texto. 
+'''
+
+cadena = input('Introduce una cadena de texto: ')
+print (cadena)
+print (cadena[::-1])
